9b48e7dcdaa2c0198afc1df9c11ecf2f1c21423d
George Barrett <georgeaustinbarrett@gmail.com>
2023-04-30 19:24:25 +0100

    integration test written for a scenario where a single Meal object is added to the menu instance array. test running GREEN

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -4,11 +4,19 @@ require 'order'
 
 RSpec.describe 'integration' do
 
-  context 'creating and adding to menu' do
+  context 'adding meal to the menu instance array' do
     it 'will fail to add a meal if it is not a Meal object' do
       menu = Menu.new
       non_existant_meal = 'test'
       expect { menu.add_menu(non_existant_meal) }.to raise_error 'That meal is not on the menu.'
+    end
+
+    it "adds a new meal to the menu instance array, with price" do
+      meal_1 = Meal.new("Gyoza", 5.99)
+      
+      menu = Menu.new
+      menu.add_menu(meal_1)
+      expect(menu.show).to eq "Gyoza - £5.99"
     end
   end
 
