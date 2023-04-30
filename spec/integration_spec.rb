b1f55969b3d1e2de1362a7193b27daae1ce3fab8
George Barrett <georgeaustinbarrett@gmail.com>
2023-04-30 19:47:16 +0100

    integration test written for a scenario where the user checks out an order with only one item in it. test running GREEN

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -85,6 +85,24 @@ RSpec.describe 'integration' do
       order.add_to_order(meal_1)
       order.checkout
     end
+
+    it "lets you checkout an order with only one item when the user confirms" do
+      io = double(:io)
+      expect(io).to receive(:puts).with("Here is your final order: ")
+      expect(io).to receive(:puts).with("Katsu Curry - £9.99\n Total: £9.99")
+      expect(io).to receive(:puts).with("To order enter Y, to cancel enter N")
+      expect(io).to receive(:gets).and_return("Y")
+      expect(io).to receive(:puts).with("Order confirmed. Your food will be delivered by drone shortly!")
+        
+      meal_1 = Meal.new("Katsu Curry", 9.99)
+      
+      menu = Menu.new
+      menu.add_menu(meal_1)
+      
+      order = Order.new(io)
+      order.add_to_order(meal_1)
+      order.checkout
+    end
   end
 
 end
