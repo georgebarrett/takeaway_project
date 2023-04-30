3d3123193e25abd9aae91867a2c7c06054ffcb46
George Barrett <georgeaustinbarrett@gmail.com>
2023-04-30 16:29:07 +0100

    'initialize' 'add_menu' 'show' methods defined in menu.rb. test written for adding a single Meal object in spec file. running GREEN

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,13 +1,17 @@
 class Menu
 
   def initialize
+    @menu = []
   end
 
   def add_menu(meal)
     fail 'That meal is not on the menu.' unless meal.is_a?(Meal)
+    @menu << meal.show_name_and_price
   end
 
   def show
+    new_menu = @menu.join(', ').tr(',', '\n')
+    return new_menu
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe Menu do
     expect { menu.add_menu("test") }.to raise_error "That meal is not on the menu."
   end
 
+  it 'adds a new dish including the price' do
+    meal_1 = double(:meal, show_name_and_price: 'Gyoza - £5.99', is_a?: true)
+
+    menu = Menu.new
+    menu.add_menu(meal_1)
+    expect(menu.show).to eq 'Gyoza - £5.99'
+  end
+
 end
