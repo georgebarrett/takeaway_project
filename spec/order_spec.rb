daca95f398c34b3f230216d9236cd461c521cd56
George Barrett <georgeaustinbarrett@gmail.com>
2023-04-30 18:57:31 +0100

    test written for a scenario where a user confirms their checkout order with only one item inside. test running GREEN

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -61,6 +61,24 @@ RSpec.describe Order do
       meal_1 = double(:dishes, show_name: "Gyoza", show_price: 6.99, show_name_and_price: "Gyoza - £6.99", is_a?: true)
       menu = double(:menu, menu: [], add_menu: true)
       menu.add_menu(meal_1)
+      
+      order = Order.new(io)
+      order.add_to_order(meal_1)
+      order.checkout
+    end
+
+    it "lets you checkout an order with one item when user confirms" do
+      io = double(:io)
+      expect(io).to receive(:puts).with("Here is your final order: ")
+      expect(io).to receive(:puts).with("Gyoza - £6.99\n Total: £6.99")
+      expect(io).to receive(:puts).with("To order enter Y, to cancel enter N")
+      expect(io).to receive(:gets).and_return("Y")
+      expect(io).to receive(:puts).with("Order confirmed. Your food will be delivered by drone shortly!")
+      
+      meal_1 = double(:dishes, show_name: "Gyoza", show_price: 6.99, show_name_and_price: "Gyoza - £6.99", is_a?: true)
+      menu = double(:menu, menu: [], add_menu: true)
+      menu.add_menu(meal_1)
+      
       order = Order.new(io)
       order.add_to_order(meal_1)
       order.checkout
