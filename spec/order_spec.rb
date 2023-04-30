ea1753290e732dbd3d70da053734ac14b50a991f
George Barrett <georgeaustinbarrett@gmail.com>
2023-04-30 18:31:17 +0100

    test written for when multiple meals get added to the basket. test running GREEN

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -21,6 +21,22 @@ RSpec.describe Order do
       order.add_to_order(meal_1)
       expect(order.show_basket).to eq "Gyoza - £5.99\n Total: £5.99"
     end
+
+    it 'adds multiple meals to the basket and displays the meals and their total price' do
+      meal_1 = double(:meal, show_name: 'Gyoza', show_price: 5.99, show_name_and_price: 'Gyoza - £5.99', is_a?: true)
+      meal_2 = double(:meal, show_name: 'Sashimi', show_price: 11.99, show_name_and_price: 'Sashimi - £11.99', is_a?: true)
+      meal_3 = double(:meal, show_name: 'Katsu Curry', show_price: 9.99, show_name_and_price: 'Katsu Curry - £9.99', is_a?: true)
+      menu = double(:menu, menu: [], add_menu: true)
+      menu.add_menu(meal_1)
+      menu.add_menu(meal_2)
+      menu.add_menu(meal_3)
+      
+      order = Order.new
+      order.add_to_order(meal_1)
+      order.add_to_order(meal_2)
+      order.add_to_order(meal_3)
+      expect(order.show_basket).to eq "Gyoza - £5.99\n Sashimi - £11.99\n Katsu Curry - £9.99\n Total: £27.97"
+    end
   end
 
 end
